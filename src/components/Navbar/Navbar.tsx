bafe7a5d0e8c8d31d3983c04e679bb0be178ece9
zak <mzhussain092@gmnail.com>
2023-05-17 15:20:15 +0530

    add auth page and styles

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+﻿import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = ({}) => {
+  return (
+    <div className="flex justify-between items-center px-2 sm:px-12 md:px-24">
+      <Link href={"/"} className="flex items-center justify-center h-20">
+        <Image
+          src={"/logo.png"}
+          alt="leetcode"
+          className="h-full"
+          width={200}
+          height={100}
+        />
+      </Link>
+      <div className="flex items-center">
+        <button className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium border-2 border-brand-orange hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange transition-all duration-300 ease-in-out">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,20 @@
+﻿import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
+import { FC } from "react";
+
+interface AuthPageProps {}
+
+const AuthPage: FC<AuthPageProps> = ({}) => {
+  return (
+    <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <Image src={"/hero.png"} alt="hero" width={750} height={500} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
