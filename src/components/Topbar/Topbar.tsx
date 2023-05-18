1f86a600b162e5edccf8883aceb8fc0acb5f83a3
zak <mzhussain092@gmnail.com>
2023-05-18 21:16:11 +0530

    add signout and reset password functionality

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,10 +1,17 @@
-﻿import Image from "next/image";
+﻿import { auth } from "@/firebase/firebase";
+import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Logout from "../Buttons/Logout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
 
 interface TopbarProps {}
 
 const Topbar: FC<TopbarProps> = ({}) => {
+  const [user] = useAuthState(auth);
+  const setAuthModalState = useSetRecoilState(authModalState);
   return (
     <>
       <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
@@ -30,20 +37,41 @@ const Topbar: FC<TopbarProps> = ({}) => {
                 Premium
               </a>
             </div>
-            <Link href="/auth">
-              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-                Sign In
-              </button>
-            </Link>
+            {!user && (
+              <Link
+                href="/auth"
+                onClick={() =>
+                  setAuthModalState((prev) => ({
+                    ...prev,
+                    open: true,
+                    type: "login",
+                  }))
+                }>
+                <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                  Sign In
+                </button>
+              </Link>
+            )}
+            {user && (
+              <div className="cursor-pointer group relative">
+                <Image
+                  src={"/avatar.png"}
+                  alt="user"
+                  width={40}
+                  height={40}
+                  className="h-8 w-8 rounded-full"
+                />
+                <div
+                  className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 
+		              transition-all duration-300 ease-in-out">
+                  <p className="text-sm">{user.email}</p>
+                </div>
+              </div>
+            )}
+            {user && <Logout />}
           </div>
         </div>
       </nav>
-
-      <div
-        className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 
-		transition-all duration-300 ease-in-out">
-        {/* <p className="text-sm">{user.email}</p> */}
-      </div>
     </>
   );
 };
