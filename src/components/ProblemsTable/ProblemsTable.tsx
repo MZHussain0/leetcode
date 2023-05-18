3fc78b85efb9c899538dc3749a36019474a65534
zak <mzhussain092@gmnail.com>
2023-05-18 17:04:06 +0530

    add homepage Problems table UI

---
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -0,0 +1,58 @@
+﻿import { problems } from "@/mockProblems/problems";
+import Link from "next/link";
+import { FC } from "react";
+
+import { AiFillYoutube } from "react-icons/ai";
+import { BsCheckCircle } from "react-icons/bs";
+
+interface ProblemsTableProps {}
+
+const ProblemsTable: FC<ProblemsTableProps> = ({}) => {
+  return (
+    <tbody className="text-white">
+      {problems.map((problem, idx) => {
+        const difficultyColor =
+          problem.difficulty === "Easy"
+            ? "text-dark-green-s"
+            : problem.difficulty === "Medium"
+            ? "text-dark-yellow"
+            : "text-dark-pink";
+        return (
+          <tr
+            key={problem.id}
+            className={`${idx % 2 === 0 ? "bg-dark-layer-1" : ""}`}>
+            <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
+              <BsCheckCircle fontSize={"18"} width="18" />
+            </th>
+            <td className="px-6 py-4">
+              <Link
+                className="hover:text-blue-600 cursor-pointer"
+                href={`/problems/${problem.id}`}>
+                {problem.title}
+              </Link>
+            </td>
+
+            <td className={`px-6 py-4 text-center ${difficultyColor}`}>
+              {problem.difficulty}
+            </td>
+
+            <td className={`px-6 py-4 text-center `}>{problem.category}</td>
+
+            <td className={`px-6 py-4  `}>
+              {problem.videoId ? (
+                <AiFillYoutube
+                  fontSize={"28"}
+                  className="cursor-pointer hover:text-pink-600"
+                />
+              ) : (
+                <p className="text-gray-400">coming soon</p>
+              )}
+            </td>
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+};
+
+export default ProblemsTable;
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,14 +1,28 @@
 ﻿import { authModalState } from "@/atoms/authModalAtom";
 import AuthModal from "@/components/Modals/AuthModal";
 import Navbar from "@/components/Navbar/Navbar";
+import { auth } from "@/firebase/firebase";
 import Image from "next/image";
-import { FC } from "react";
+import { useRouter } from "next/router";
+import { FC, useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilValue } from "recoil";
 
 interface AuthPageProps {}
 
 const AuthPage: FC<AuthPageProps> = ({}) => {
   const authModal = useRecoilValue(authModalState);
+  const [user, loading, error] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, loading, router]);
+
+  if (pageLoading) return null;
+
   return (
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
