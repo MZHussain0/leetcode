a822a8b7ceed83a3053c254eb3b882a3e706ebf9
zak <mzhussain092@gmnail.com>
2023-05-18 21:16:39 +0530

    add signout and reset password funtionality

---
+++ src/components/Buttons/Logout.tsx
@@ -0,0 +1,22 @@
+﻿import { auth } from "@/firebase/firebase";
+import { FC } from "react";
+import { useSignOut } from "react-firebase-hooks/auth";
+import { FiLogOut } from "react-icons/fi";
+
+interface LogoutProps {}
+
+const Logout: FC<LogoutProps> = ({}) => {
+  const [signOut, loading, error] = useSignOut(auth);
+  const handleLogout = () => {
+    signOut();
+  };
+  return (
+    <button
+      className="bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange"
+      onClick={handleLogout}>
+      <FiLogOut />
+    </button>
+  );
+};
+
+export default Logout;
